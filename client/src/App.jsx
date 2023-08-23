d48fa09e70090cb668cbcbbc8bde263481572ad2
Your Name <wallicestenewaweru@gmail.com>
2023-08-23 13:06:29 +0300

    shortUrl

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home from './Home'
-
+import {Toaster} from "react-hot-toast" 
 function App() {
   return (
     <div>
       <Home/>
+      <Toaster
+  position="top-center"
+  reverseOrder={false}
+/>
     </div>
   )
 }
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,25 +1,31 @@
 import React, { useEffect, useState } from "react";
 import pic1 from "./assets/pic1.jpeg";
+import { toast } from "react-hot-toast";
 function Home() {
   const [longUrl, setlongUrl] = useState("");
-  const [longData, setlongData] = useState([]);
   const [ShortUrl, setShortUrl] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    const input = { longUrl };
-    fetch("http://localhost:3000/urls", {
-      method: "POST",
-      body: JSON.stringify(input),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-    .then(result => result.json())
-    .then(data => {
-        console.log(data);
-        setShortUrl(data.shortUrl)
-    })
-    setlongUrl("")
+    if (longUrl) {
+      fetch("http://localhost:3000/urls", {
+        method: "POST",
+        body: JSON.stringify({ longUrl }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((result) => result.json())
+        .then((data) => {
+          console.log(data);
+          setShortUrl(data.shortUrl);
+        });
+    }
+    setlongUrl("");
+  };
+  const copy = () => {
+    navigator.clipboard.writeText(`${ShortUrl}`).then(() => {
+      toast.success("Copied to clipboard");
+    });
   };
   return (
     <div className=" relative h-screen flex flex-col items-center object-center bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
@@ -35,37 +41,43 @@ function Home() {
         <div>
           <form
             onSubmit={handleSubmit}
-            className=" bg-white py-3 px-2 w-full flex flex-col items-center gap-4 rounded-md"
+            className=" bg-white shadow-lg py-3 px-2 w-full flex flex-col items-center gap-4 rounded-md"
           >
             <div className=" w-full flex items-center gap-4">
-                <input
-              type="url"
-              onChange={(e) => setlongUrl(e.target.value)}
-              value={longUrl}
-              placeholder="Enter the link here"
-              className=" indent-1 w-full h-8 outline-none bg-none border border-black rounded"
-            />
-            <button className=" bg-black text-white py-1 px-4 rounded">
-              Shorten
-            </button>
+              <input
+                type="url"
+                onChange={(e) => setlongUrl(e.target.value)}
+                value={longUrl}
+                placeholder="Enter the link here"
+                className=" indent-1 w-full h-8 outline-none bg-none border border-black rounded"
+              />
+              <button className=" bg-black text-white py-1 px-4 rounded">
+                Shorten
+              </button>
             </div>
-            {
-                ShortUrl && (
-                    <div className=" flex w-full items-center gap-4">
-                        Your tiny Url :
-                    <input 
+            {ShortUrl && (
+              <div className=" w-full">
+                <p htmlFor="shortUrl" className=" inline-block">
+                  Your TinyUrl:
+                </p>
+                <div className=" my-1 flex w-full items-center gap-4">
+                  <input
+                    id="shortUrl"
                     value={ShortUrl}
                     readOnly
-                    className=" indent-1 h-8 outline-none bg-none border border-black rounded"
-                    />
-                     <a href={`http://${ShortUrl}`} target="_blank">{`http://${ShortUrl}`}</a>
-                    </div>
-                )
-            }
+                    className=" indent-1 h-8 w-full outline-none bg-none border border-black rounded"
+                  />
+                  <button
+                    onClick={copy}
+                    className=" bg-black text-white py-1 px-6 rounded"
+                  >
+                    Copy
+                  </button>
+                </div>
+              </div>
+            )}
           </form>
-           <div>
-           
-           </div>
+          <div></div>
         </div>
       </div>
     </div>
