f946b9c9d22aecffd1c4291f7f39facd23bd9859
Your Name <wallicestenewaweru@gmail.com>
2023-08-28 11:04:55 +0300

    card

--- client/src/FooterCard.jsx
+++ client/src/FooterCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FooterCard() {
   return (
-        <footer className="bg-white p-1 rounded-md text-center text-xs font-Raleway">
+        <footer className="bg-white p-1 rounded-md text-center text-xs font-Raleway shadow-xl border border-slate-800">
             <div>
                 <p>Made with ❤️ by <br /> Wallicestene Waweru</p>
                 <a href="http://github.com/wallicestene">
